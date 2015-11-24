ef6f7673c6c7719875963ad063c951802fcba715
Jeffrey Regier <jeff@stat.berkeley.edu>
2015-11-23 17:08:14 -0800

    changing license

--- authortoolkit/cluster.py
+++ authortoolkit/cluster.py
@@ -1,21 +1,3 @@
-# Copyright 2009, Jeffrey Regier, jeff [at] stat [dot] berkeley [dot] edu
-
-# This file is part of the Badger Author Disambiguation Toolkit (Badger).
-#
-# Badger is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Badger is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Badger.  If not, see <http://www.gnu.org/licenses/>.
-
-
 from collections import defaultdict
 import utils
 from mention import Mention
--- authortoolkit/nick_names.py
+++ authortoolkit/nick_names.py
@@ -1,21 +1,3 @@
-# Copyright 2008, Jeffrey Regier, jeff [at] stat [dot] berkeley [dot] edu
-
-# This file is part of Author-Dedupe.
-#
-# Author-Dedupe is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Author-Dedupe is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Author-Dedupe.  If not, see <http://www.gnu.org/licenses/>.
-
-
 nick_names = {
     "al": "albert",
     "andy": "andrew",
